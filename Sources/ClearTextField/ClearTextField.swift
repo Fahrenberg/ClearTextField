de566bf088692bcc2e04cb93dca78ddc17405599
Jean-Nicolas <70505484+Fahrenberg@users.noreply.github.com>
2022-07-17 22:12:56 +0200

    1.0 - alignment right wrong

--- Sources/ClearTextField/ClearTextField.swift
+++ Sources/ClearTextField/ClearTextField.swift
@@ -1,6 +1,31 @@
-public struct ClearTextField {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+// UI Examples
+struct ClearTextField: View {
+    @State private var s: String = "ClearTextField"
+    @State private var d: Double = 11.5
+    var body: some View {
+        VStack {
+            Group {
+                TextField("String", text: $s)
+                    .clearStringButton($s)
+                ClearNumberTextField(value: $d)
+                    .multilineTextAlignment(.trailing)
+            }
+            .background(.gray)
+            .textFieldStyle(.roundedBorder)
+        }
+        .padding(20)
+        .background(.yellow)
+        
     }
 }
+
+
+struct ClearTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        ClearTextField()
+           .previewLayout(.sizeThatFits)
+    }
+}
+
